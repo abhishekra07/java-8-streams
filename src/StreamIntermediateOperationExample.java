0c172471295835d62e1775a4a817d3ee82e72c93
abhishekra07 <abhira719@gmail.com>
2023-04-28 17:23:26 +0530

    Update StreamIntermediateOperationExample.java

--- src/StreamIntermediateOperationExample.java
+++ src/StreamIntermediateOperationExample.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamIntermediateOperationExample {
     public static void main(String[] args) {
-        
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("Abhishek", 24));
+        studentList.add(new Student("Rahul", 25));
+        studentList.add(new Student("Ajay", 22));
+        studentList.add(new Student("Sanjay", 32));
+        List<String> names = studentList.stream().map(student -> student.name).collect(Collectors.toList());
+        names.forEach(name -> System.out.println(name));
+    }
+}
+
+class Student {
+    public String name;
+    public int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
