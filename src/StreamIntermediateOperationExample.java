2b6a97fe89c7502626a1d3bf475cef6e619331c7
abhishekra07 <abhira719@gmail.com>
2023-04-28 17:28:00 +0530

    Update StreamIntermediateOperationExample.java

--- src/StreamIntermediateOperationExample.java
+++ src/StreamIntermediateOperationExample.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,8 +11,26 @@ public class StreamIntermediateOperationExample {
         studentList.add(new Student("Rahul", 25));
         studentList.add(new Student("Ajay", 22));
         studentList.add(new Student("Sanjay", 32));
+
+        //Map usage
         List<String> names = studentList.stream().map(student -> student.name).collect(Collectors.toList());
         names.forEach(name -> System.out.println(name));
+
+        //flatMap Example
+        List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5),
+                Arrays.asList(6, 7, 8, 9, 10)
+        );
+
+        List<Integer> flatList = listOfLists.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        flatList.forEach(num -> System.out.println(num));
+//        List<Integer> flatList = listOfLists.stream()
+//                .flatMap(e -> e.stream())
+//                .collect(Collectors.toList());
+//        flatList.forEach(num -> System.out.println(num));
     }
 }
 
