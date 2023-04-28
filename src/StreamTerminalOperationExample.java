8bec2f641904e0c9905a2bdeee3e177f91d4e6b7
abhishekra07 <abhira719@gmail.com>
2023-04-28 17:50:49 +0530

    Update StreamTerminalOperationExample.java

--- src/StreamTerminalOperationExample.java
+++ src/StreamTerminalOperationExample.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class StreamTerminalOperationExample {
     public static void main(String[] args) {
-        
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        Optional<Integer> total = numbers.stream().reduce((num, sum) -> sum + num);
+        System.out.println(total.get());
     }
 }
