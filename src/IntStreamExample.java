0964a1bf6f83765e082f015ad22e34be6aa72f2d
abhishekra07 <abhira719@gmail.com>
2023-05-01 09:39:24 +0530

    Update IntStreamExample.java

--- src/IntStreamExample.java
+++ src/IntStreamExample.java
@@ -1,5 +1,11 @@
+import java.util.stream.IntStream;
+
 public class IntStreamExample {
     public static void main(String[] args) {
-        
+        //IntStream.of Example
+        IntStream singleValue = IntStream.of(10);
+        singleValue.forEach(num -> System.out.println(num));
+        IntStream multipleValues = IntStream.of(1, 5, 10, 20, 30);
+        multipleValues.forEach(num -> System.out.println(num));
     }
 }
