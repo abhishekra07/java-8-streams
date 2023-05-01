8b60e29260c548e28c4321e978a41ea16cc9c87c
abhishekra07 <abhira719@gmail.com>
2023-05-01 09:40:24 +0530

    Update IntStreamExample.java

--- src/IntStreamExample.java
+++ src/IntStreamExample.java
@@ -7,5 +7,10 @@ public class IntStreamExample {
         singleValue.forEach(num -> System.out.println(num));
         IntStream multipleValues = IntStream.of(1, 5, 10, 20, 30);
         multipleValues.forEach(num -> System.out.println(num));
+
+        //IntStream.range()
+        //range() is used to generate the numbers in the order with incremental by one
+        IntStream range10to30 = IntStream.range(10, 20); //last value not included
+        range10to30.forEach(num -> System.out.println(num));
     }
 }
